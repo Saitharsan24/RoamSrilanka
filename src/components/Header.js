5208f1581138212c91e4036c39f0e12dbfcbb39e
Saitharsan Nanthakumaran <118580513+Saitharsan24@users.noreply.github.com>
2023-07-14 20:48:06 +0530

    Development (#5)
    * footer component patially finished
    
    * Started to do blog page
    
    * done header component part (#4)
    
    Co-authored-by: laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: R Saranjan <ragunathansaranjan@gmail.com>
    Co-authored-by: R Saranjan <108950430+SRKSaran@users.noreply.github.com>
    Co-authored-by: laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import systemlogo from './../assets/images/systemlogo.png';
+import '../styles/Header.css';
+import * as Icon from 'react-bootstrap-icons';
+import { Form } from 'react-bootstrap';
+
+const Header=()=>{
+    return(
+        <Navbar className='nav' expand="lg">
+      <React.Fragment className="nav-div">
+      
+        <Navbar.Toggle  aria-controls="responsive-navbar-nav"/>
+              <Navbar.Collapse id="responsive-navbar-nav">
+              <img className='logo' src={systemlogo}  />
+
+            <Nav className="ms-auto nav-item-list-1">
+            <Nav.Link className='text-black m-2 jost-text nav-item-1 text-decoration-underline-hover' href="#home">Home</Nav.Link>
+            <Nav.Link className='text-black m-2 jost-text nav-item-1' href="#link">About</Nav.Link>
+            <Nav.Link className='text-black m-2 jost-text nav-item-1' href="#link">Services</Nav.Link>
+            <Nav.Link className='text-black m-2 jost-text nav-item-1' href="#link">Explore</Nav.Link>
+            <Nav.Link className='text-black m-2 jost-text nav-item-1' href="#link">Blogs</Nav.Link>
+            <Nav.Link className='text-black m-2 jost-text nav-item-1' href="#link">Contact</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto nav-item-list-2">
+            <Nav.Link className='text-black m-2 jost-text nav-item-2' href="#link">Log In</Nav.Link>
+            <Nav.Link className='text-danger m-2 jost-text nav-item-2' href="#link"><button type="button" className="btn btn-danger reg-btn">Register</button></Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </React.Fragment>
+    </Navbar>
+    );
+};
+
+export default Header
