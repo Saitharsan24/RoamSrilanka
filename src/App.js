69f7c6ba79d7dd5692d88970b2411381578f5f77
laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>
2023-07-16 12:45:13 +0530

    done some part of About page,some changes  in header part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/footer';
+import About from './pages/about'
 
 function App() {
   return (
-   <Header />
+    <React.Fragment>
+    <About/>
+    </React.Fragment>
+  
   );
 }
 
