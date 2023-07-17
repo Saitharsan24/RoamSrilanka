803d0d018543e5cd52c3e865b137bafbbe7d7a3d
saiNan24 <118580513+Saitharsan24@users.noreply.github.com>
2023-07-17 12:38:55 +0530

    edited changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/footer';
-import FlashPage from './components/flashPage';
 
 function App() {
   return (
     <div>
-      <Header />
-      <FlashPage></FlashPage>
-      <Footer />
       
     </div>
   );
