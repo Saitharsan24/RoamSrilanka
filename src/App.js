8185336dcd7384eb92ae8ffa18452799f9e5d67d
R Saranjan <ragunathansaranjan@gmail.com>
2023-07-19 17:50:03 +0530

    Hotel's sidebar component is finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { Sidebar } from "react-pro-sidebar";
 import Header from './components/Header';
 import Footer from './components/footer';
 import Contact from './pages/Contact';
 import Service from './pages/services';
 import Routing from './routes/Routing';
-
+import Hotel from './components/sidebar';
 function App() {
   return (
-    <Routing />
+    <Hotel/>
   );
 }
 
