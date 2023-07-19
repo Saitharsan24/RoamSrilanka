0972e7c68c43417c65a2a83e020511be22196eb8
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2023-07-19 22:23:02 +0530

    7/19
    holiday planner dash board created

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Contact from './pages/Contact';
 import Service from './pages/services';
 import Blog from './pages/blog';
 import Routing from './routes/Routing';
+import HolidayDashboard from './pages/holidayplanner_dashboard';
 
 function App() {
   return (
-    <Routing />
+    <Routing/>
   );
 }
 
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -6,6 +6,7 @@ import Contact from '../pages/Contact';
 import Services from '../pages/services';
 import Blog from '../pages/blog';
 import About from '../pages/about';
+import HolidayDashboard from '../pages/holidayplanner_dashboard';
 
 function Routing() {
   return (
@@ -16,6 +17,7 @@ function Routing() {
             <Route path='/Services' element={<Services />}></Route>
             <Route path='/Blog' element={<Blog />}></Route>
             <Route path='/About' element={<About />}></Route>
+            <Route path='/Holidash' element={<HolidayDashboard />}></Route>
         </Routes>
     </BrowserRouter>
   )
