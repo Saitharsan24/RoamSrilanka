53d2f0e83ef83e6d0084ac5f3a83975b068e0b54
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2023-07-20 13:47:47 +0530

    7/20
    holiday planner profile page responsively finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 import { Sidebar } from "react-pro-sidebar";
-import Header from './components/Header';
-import Footer from './components/footer';
-import Contact from './pages/Contact';
-import Service from './pages/services';
-import Blog from './pages/blog';
-import Routing from './routes/Routing';
-import HolidayDashboard from './pages/holidayplanner_dashboard';
+import Header from "./components/Header";
+import Footer from "./components/footer";
+import Contact from "./pages/Contact";
+import Service from "./pages/services";
+import Blog from "./pages/blog";
+import Routing from "./routes/Routing";
+import Holidayprofile from "./pages/holidayplanner_profile";
 
 function App() {
-  return (
-    <Routing/>
-  );
+  return <Routing />;
 }
 
 export default App;
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from '../components/Header';
-import Contact from '../pages/Contact';
-import Services from '../pages/services';
-import Blog from '../pages/blog';
-import About from '../pages/about';
-import Login from '../pages/login';
-import HolidayDashboard from '../pages/holidayplanner_dashboard';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "../components/Header";
+import Contact from "../pages/Contact";
+import Services from "../pages/services";
+import Blog from "../pages/blog";
+import About from "../pages/about";
+import Login from "../pages/login";
+import Holidayprofile from "../pages/holidayplanner_profile";
 
 function Routing() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Header />}></Route>
-            <Route path='/Contact' element={<Contact />}></Route>
-            <Route path='/Services' element={<Services />}></Route>
-            <Route path='/Blog' element={<Blog />}></Route>
-            <Route path='/About' element={<About />}></Route>
-            <Route path='/Login' element={<Login />}></Route>
-            <Route path='/Holidash' element={<HolidayDashboard />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Header />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/Services" element={<Services />}></Route>
+        <Route path="/Blog" element={<Blog />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Holidayprofile" element={<Holidayprofile />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Routing
+export default Routing;
