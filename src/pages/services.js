2c35919e5870650d30f8422dedaf1f8cc22a73b2
Saitharsan Nanthakumaran <118580513+Saitharsan24@users.noreply.github.com>
2023-07-24 15:24:43 +0530

    Sai (#28)
    * Development (#5)
    
    * footer component patially finished
    
    * Started to do blog page
    
    * done header component part (#4)
    
    Co-authored-by: laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: R Saranjan <ragunathansaranjan@gmail.com>
    Co-authored-by: R Saranjan <108950430+SRKSaran@users.noreply.github.com>
    Co-authored-by: laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>
    
    * created flash page
    
    * editied flash page
    
    * edited changes
    
    * edited Sai branch
    
    * created landing page
    
    * edited explore page
    
    ---------
    
    Co-authored-by: R Saranjan <ragunathansaranjan@gmail.com>
    Co-authored-by: R Saranjan <108950430+SRKSaran@users.noreply.github.com>
    Co-authored-by: laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>

--- src/pages/services.js
+++ src/pages/services.js
@@ -17,6 +17,7 @@ import backpack from "../assets/images/backpack 1.png";
 function Services() {
   return (
     <React.Fragment>
+
       {<Header />}
       <div className="top">
         <p className="text-white text-center text_1 align-middle ">Services</p>
@@ -94,7 +95,6 @@ function Services() {
           </div>
         </div>
       </div>
-
       <Footer />
     </React.Fragment>
   );
