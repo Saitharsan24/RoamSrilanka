484be268a172ef9604d63f94cc476f6288dfd5f6
saiNan24 <saitharsan@gmail.com>
2023-08-18 14:50:06 +0530

    did changes to signup

---
+++ back-end/src/main/java/roamSrilanka/dev/controller/TouristController.java
@@ -0,0 +1,11 @@
+package roamSrilanka.dev.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+
+@RestController
+public class TouristController {
+}
