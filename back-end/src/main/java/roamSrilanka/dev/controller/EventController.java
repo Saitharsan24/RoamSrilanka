76fd8de567b613e80a688872252e32f8b3cc5423
KeethapriyaM <keethapriya.m@gmail.com>
2023-08-26 23:28:05 +0530

    created CRUD for events

---
+++ back-end/src/main/java/roamSrilanka/dev/controller/EventController.java
@@ -0,0 +1,40 @@
+package roamSrilanka.dev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import roamSrilanka.dev.model.Event;
+import roamSrilanka.dev.service.EventService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/getAllEvents")
+    public List<Event> getAllEvents() {
+        return this.eventService.getAllEvents();
+    }
+
+    @PostMapping("/addEvent")
+    public Event createEvent(@RequestBody Event newEvent) {
+        return this.eventService.createEvent(newEvent);
+    }
+
+    @GetMapping("/getParticularEvent/{id}")
+    public Optional<Event> getEvent (@PathVariable(value = "id") Integer identity ) {
+        return this.eventService.getEvent(identity);
+    }
+
+//    @PutMapping("/updateEvent/{id}")
+//    public Event updateEvent(@PathVariable(value = "id") Integer identity, @RequestBody Event eventRequest) {
+//        return this.eventService.updateEvent(identity, eventRequest);
+//    }
+
+    @DeleteMapping("/deleteEvent/{id}")
+    public String deleteEvent (@PathVariable(value = "id") Integer identity) {
+        return this.eventService.deleteEvent(identity);
+    }
+}
---
+++ back-end/src/main/java/roamSrilanka/dev/service/EventService.java
@@ -0,0 +1,40 @@
+package roamSrilanka.dev.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import roamSrilanka.dev.model.Event;
+import roamSrilanka.dev.repository.EventRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventService {
+    @Autowired
+    private EventRepository eventRepository;
+
+    public List<Event> getAllEvents() {
+        return this.eventRepository.findAll();
+    }
+
+    public Event createEvent (Event newEvent) {
+        return this.eventRepository.save(newEvent);
+    }
+
+    public Optional<Event> getEvent(Integer id) {
+        return this.eventRepository.findById(id);
+    }
+
+//    public Event updateEvent(int id, Event eventRequest) {
+//        Event existingEvent = eventRepository.findById(id).get();
+//        existingEvent.setName(eventRequest.getName());
+//        existingEvent.setDate(eventRequest.getDate());
+//        existingEvent.setPlaces(eventRequest.getPlaces());
+//        existingEvent.setDays(eventRequest.getDays());
+//        return eventRepository.save(existingEvent);
+//    }
+    public String deleteEvent (Integer id) {
+        this.eventRepository.deleteById(id);
+        return "Successfully Deleted";
+    }
+}
