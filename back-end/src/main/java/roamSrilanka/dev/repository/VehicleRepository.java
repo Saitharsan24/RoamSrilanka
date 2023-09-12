f7e5597dbbfd539c830834dc8f58431b594df51f
Laxshan <108828860+Laxshan19@users.noreply.github.com>
2023-09-12 16:55:24 +0530

    Lax (#76)
    * holliday planner backend
    
    * create vehicle repository

---
+++ back-end/src/main/java/roamSrilanka/dev/repository/VehicleRepository.java
@@ -0,0 +1,7 @@
+package roamSrilanka.dev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import roamSrilanka.dev.model.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
