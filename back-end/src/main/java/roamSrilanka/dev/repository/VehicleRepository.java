bd87b9648dd84fe1fa99676f5d0735410b1c7a6e
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2023-09-21 20:19:41 +0530

    vehicle from driver

--- back-end/src/main/java/roamSrilanka/dev/repository/VehicleRepository.java
+++ back-end/src/main/java/roamSrilanka/dev/repository/VehicleRepository.java
@@ -1,7 +1,19 @@
 package roamSrilanka.dev.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import roamSrilanka.dev.model.Vehicle;
 
+import java.util.List;
+
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+
+
+    @Query(value = "SELECT COUNT(*) FROM vehicle", nativeQuery = true)
+    Long getVehicleCount();
+
 }
