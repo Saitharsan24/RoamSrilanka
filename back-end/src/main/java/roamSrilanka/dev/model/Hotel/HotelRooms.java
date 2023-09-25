011fba9145a467c4f34af06609bba03a288da934
R Saranjan <108950430+SRKSaran@users.noreply.github.com>
2023-09-25 11:25:36 +0530

    Some hotel parts changes

--- back-end/src/main/java/roamSrilanka/dev/model/Hotel/HotelRooms.java
+++ back-end/src/main/java/roamSrilanka/dev/model/Hotel/HotelRooms.java
@@ -18,10 +18,8 @@ public class HotelRooms {
 //    @JoinColumn(name = "hotel_id", nullable = false, insertable = false, updatable = false)
 //    private Hotels hotel; // Reference to the hotel this room belongs to
 
-    @ManyToOne
-    @MapsId
     @JoinColumn(name = "hotel_id")
-    private Hotels hotels;
+    private Integer hotelId;
 
     @Column(name = "room_type")
     private String roomType;
@@ -63,7 +61,7 @@ public class HotelRooms {
 
     public HotelRooms(Integer roomId, Integer hotelId, String roomType, Integer occupancy, Double price, Integer noOfBeds, String policyBathroom, String policyBedroom, String policyFoodanddrink, String policyInternet, List<String> amenities, List<String> images) {
         this.roomId = roomId;
-        this.hotels = hotels;
+        this.hotelId = hotelId;
         this.roomType = roomType;
         this.occupancy = occupancy;
         this.price = price;
@@ -90,16 +88,9 @@ public class HotelRooms {
         this.images = images;
     }
 
-    public HotelRooms(String roomType, Integer occupancy, Double price, Integer noOfBeds, List<String> amenities, List<String> images) {
-        this.roomType = roomType;
-        this.occupancy = occupancy;
-        this.price = price;
-        this.noOfBeds = noOfBeds;
-        this.amenities = amenities;
-        this.images = images;
-    }
-
     //Getters and Setters
+
+
     public Integer getRoomId() {
         return roomId;
     }
@@ -108,14 +99,45 @@ public class HotelRooms {
         this.roomId = roomId;
     }
 
-//    public Hotels getHotel() {
-//        return hotel;
-//    }
-//
-//    public void setHotel(Hotels hotel) {
-//        this.hotel = hotel;
-//    }
+    public Integer getHotelId() {
+        return hotelId;
+    }
 
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public Integer getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(Integer occupancy) {
+        this.occupancy = occupancy;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getNoOfBeds() {
+        return noOfBeds;
+    }
+
+    public void setNoOfBeds(Integer noOfBeds) {
+        this.noOfBeds = noOfBeds;
+    }
 
     public String getPolicyBathroom() {
         return policyBathroom;
@@ -147,46 +169,6 @@ public class HotelRooms {
 
     public void setPolicyInternet(String policyInternet) {
         this.policyInternet = policyInternet;
-    }
-
-    public Integer getHotelId() {
-        return hotelId;
-    }
-
-    public void setHotels(Hotels hotels) {
-        this.hotels = hotels;
-    }
-
-    public String getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-
-    public Integer getOccupancy() {
-        return occupancy;
-    }
-
-    public void setOccupancy(Integer occupancy) {
-        this.occupancy = occupancy;
-    }
-
-    public void setNoOfBeds(Integer noOfBeds) {
-        this.noOfBeds = noOfBeds;
-    }
-
-    public Integer getNoOfBeds() {
-        return noOfBeds;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
     }
 
     public List<String> getAmenities() {
